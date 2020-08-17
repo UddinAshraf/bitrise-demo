c0a677b1241e3d766f685ac15598d12ab2a3e443
Ashraf Uddin <ashraf@tise.com>
2020-08-17 09:47:47 +0600

    Initial Commit

---
+++ BitriseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Nahid on 17/8/20.
+//  Copyright © 2020 Nahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
